df6b17d9139f6e733adc6ffa193c46078773b085
mayamariakjaer <maya_t_kjaer@hotmail.com>
2020-03-09 22:29:49 +0100

    Initial commit

---
+++ app/src/main/java/com/example/forecastmvvmapp/Ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.forecastmvvmapp.Ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.forecastmvvmapp.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        //henter ut id via activity main
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        bottom_nav.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(null, navController)
+    }
+}
---
+++ app/src/main/java/com/example/forecastmvvmapp/Ui/weather/future/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.forecastmvvmapp.Ui.weather.future
+
+import androidx.lifecycle.ViewModel
+
+class FutureDetailWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
